28389fdcee114dff3a927b07100e8a774fba3f4a
Caleb Chun <chuncaleb0@gmail.com>
2023-11-21 17:10:55 -0800

    Incomplete

--- Student.h
+++ Student.h
@@ -1,22 +1,25 @@
 #include "Course.h"
 #include <string>
+#include <vector>
 using namespace std;
 
-class Student {
-    private: 
-        string name;
-        int studentID;
-        vector<Course *> courses;
-    
-    public: 
-        friend ostream& operator<<(ostream& out, const Student &student);
-        
-        // Drop student from given course, return true if successful
-        bool dropCourse(int studentID, const string &courseNumber);
+class Student
+{
+private:
+    string firstName;
+    string lastName;
+    int studentID;
+    vector<Course *> courses;
 
-        // Add student to the given course, return true if successful
-        bool addCourse(int studentID, const string &courseNumber);
+public:
+    friend ostream &operator<<(ostream &out, const Student &student);
 
-        // Return true if student is in the given course
-        bool isInCourse(int studentID, const string &courseNumber) const;
+    // Drop student from given course, return true if successful
+    bool dropCourse(int studentID, const string &courseNumber);
+
+    // Add student to the given course, return true if successful
+    bool addCourse(int studentID, const string &courseNumber);
+
+    // Return true if student is in the given course
+    bool isInCourse(int studentID, const string &courseNumber) const;
 };
