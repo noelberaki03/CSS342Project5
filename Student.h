2fce653a7601e6fe53bc949fde5cd21a2487dc8f
Caleb Chun <chuncaleb0@gmail.com>
2023-11-28 14:44:31 -0800

    Peer Review Needed: Caleb has Reviewed

--- Student.h
+++ Student.h
@@ -1,14 +1,14 @@
 /* Description of Student class
  *
- * This class represents a student entity with a student ID, 
+ * This class represents a student entity with a student ID,
  * a first and last name, and a list of courses they are currently enrolled in.
  *
  * The class allows for a student to view their enrollment information,
- * add a course to their schedule, drop a course, and check if they are 
+ * add a course to their schedule, drop a course, and check if they are
  * currently registered for a course.
-*/
+ */
 
-#ifnef STUDENT_H
+#ifndef STUDENT_H
 #define STUDENT_H
 
 #include "Course.h"
@@ -17,34 +17,38 @@
 
 using namespace std;
 
-class Student {
+class Student
+{
     friend class Course;
     friend class University;
 
     // prints students information including name, id, and currently enrolled courses
-    friend ostream& operator<<(ostream &out, const Student &student);
+    friend ostream &operator<<(ostream &out, const Student &student);
 
-    private:
-        int studentId;
-        string firstName;
-        string lastName;
-        map<string, Course*> courses;
+private:
+    int studentId;
+    string firstName;
+    string lastName;
+    map<string, Course *> courses;
 
-    public:
-        // constructor
-        Student(int id, const string &fName, const string &lName);
+    // constructor
+    Student(int id, const string &fName, const string &lName);
 
-        // destructor
-        ~Student();
+    // destructor
+    ~Student();
 
-        // drops student from given course specified by course number. Returns true if successful
-        bool dropCourse(const string &courseNumber);
+    // drops student from given course specified by course number. Returns true if successful
+    bool dropCourse(const string &courseNumber);
 
-        // adds student to the given course specified by course number. Returns true if successful
-        bool addCourse(const string &courseNumber);
+    // adds student to the given course specified by course number. Returns true if successful
+    bool addCourse(const string &courseNumber);
 
-        // returns true if student is in the given course
-        bool isInCourse(const string &courseNumber);
+    // returns true if student is in the given course
+    bool isInCourse(const string &courseNumber);
+
+    // Return the courses student is enrolled in
+    // The returned courses are separated by commas and sorted by course name
+    string getEnrolledCourses(int studentID) const;
 };
 
 #endif
