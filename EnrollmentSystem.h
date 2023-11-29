443cd1f43deab9f41c3dc5f6816fb7d5e6503764
Caleb Chun <chuncaleb0@gmail.com>
2023-11-28 19:52:14 -0800

    Final Touches

--- EnrollmentSystem.h
+++ EnrollmentSystem.h
@@ -11,7 +11,15 @@ using namespace std;
 class EnrollmentSystem{
     private:
     vector<University*> universities;
+    
     public:
+
+    //constructor
+    EnrollmentSystem();
+
+    // deconstructor
+    ~EncrollmentSystem();
+
     // Add university to the enrollments system
     bool addUniversity(const string &name);
 
@@ -20,61 +28,5 @@ class EnrollmentSystem{
 
     // Return the current active university name
     string getUniversityName() const;
-
-    // Read the student list for current active university
-     // return true if file successfully read
-    bool readStudentList(const string &filename);
-
-    /**
-     * Reads in a text file of courseIDs and course names and 
-     * stores them as Course objects in the University courseList.
-    */
-    // Read the course list for current active university
-    // return true if file successfully read
-    bool readCourseList(const string &filename);
-
-    /**
-     * Reads in a text file of studentID numbers and courseIDs 
-     * and adds those students to the course objects classList 
-     * and the corresponding student objects courses.
-    */
-    // Read the student enrollment information for current active university
-    // return true if file successfully read
-    bool readEnrollmentInfo(const string &filename);
-
-    // Drop student from given course, return true if successful
-    bool dropCourse(int studentID, const string &courseNumber);
-
-    /**
-     * Adds a course to a students “courses” list and 
-     * adds that student object to the Course objects classList.
-    */
-    // Add student to the given course, return true if successful
-    bool addCourse(int studentID, const string &courseNumber);
-
-    // Return true if student is in the given course
-    bool isInCourse(int studentID, const string &courseNumber) const;
-
-    /**
-     * Returns all of the courseIDs in the student’s “courses” list as a string.
-    */
-    // Return the courses student is enrolled in
-    // The returned courses are separated by commas and sorted by course name
-    string getEnrolledCourses(int studentID) const;
-
-    // Return the title for the course
-    string getCourseTitle(const string &courseNumber);
-
-    /**
-     * Takes in a string courseNumber and looks through the courseList 
-     * for the corresponding courseID. Finds the course pointer/courseID 
-     * and compares the student’s last names with cmpLastName to sort alphabetically 
-     * and stores that into a string to return.
-    */
-    // Return class list sorted by last name of students
-    string getClassListByLastName(const string &courseNumber) const;
-
-    // Return class list sorted by id of students
-    string getClassListByID(const string &courseNumber) const;
 };
 
--- University.h
+++ University.h
@@ -8,33 +8,73 @@ It has methods to read in a list of courses, read in a list of students, read in
 #include "Course.h"
 #include <map>
 
-
-class University{
+class University
+{
   friend class EnrollmentSystem;
-  private:
-  string name;
-  unordered_map <string, Course*> courseList;
 
-  //Constructor for University 
+private:
+  string name;
+  unordered_map<string, Course *> courseList;
+
+  // Constructor for University
   University(string name);
 
-  //Desrtuctor for University
+  // Desrtuctor for University
   ~University();
 
-  //reads in the student list, by ID number then name, returns true if file is read successfully
-  bool readStudentList(const string& fileName);
+  // Read the student list for current active university
+  // return true if file successfully read
+  bool readStudentList(const string &filename);
 
-  //reads in the course list returns true if file is read successfully
-  bool readCourseList(const string& fileName);
+  /**
+   * Reads in a text file of courseIDs and course names and
+   * stores them as Course objects in the University courseList.
+   */
+  // Read the course list for current active university
+  // return true if file successfully read
+  bool readCourseList(const string &filename);
 
-  //reads the current student enrollment info for current active university
-  // returns true if file is read successfully
-  bool readEnrollmentInfo(const string& fileName);
+  /**
+   * Reads in a text file of studentID numbers and courseIDs
+   * and adds those students to the course objects classList
+   * and the corresponding student objects courses.
+   */
+  // Read the student enrollment information for current active university
+  // return true if file successfully read
+  bool readEnrollmentInfo(const string &filename);
 
-  //Adds a student to the specified course, return true if successful
-  bool addCourse(int studentID, const string& courseNumber);
+  // Drop student from given course, return true if successful
+  bool dropCourse(int studentID, const string &courseNumber);
 
-  //Drops a student to the specified course, return true if successful
-  bool dropCourse(int studentID, const string& courseNumber);
+  /**
+   * Adds a course to a students “courses” list and
+   * adds that student object to the Course objects classList.
+   */
+  // Add student to the given course, return true if successful
+  bool addCourse(int studentID, const string &courseNumber);
 
+  // Return true if student is in the given course
+  bool isInCourse(int studentID, const string &courseNumber) const;
+
+  /**
+   * Returns all of the courseIDs in the student’s “courses” list as a string.
+   */
+  // Return the courses student is enrolled in
+  // The returned courses are separated by commas and sorted by course name
+  string getEnrolledCourses(int studentID) const;
+
+  // Return the title for the course
+  string getCourseTitle(const string &courseNumber);
+
+  /**
+   * Takes in a string courseNumber and looks through the courseList
+   * for the corresponding courseID. Finds the course pointer/courseID
+   * and compares the student’s last names with cmpLastName to sort alphabetically
+   * and stores that into a string to return.
+   */
+  // Return class list sorted by last name of students
+  string getClassListByLastName(const string &courseNumber) const;
+
+  // Return class list sorted by id of students
+  string getClassListByID(const string &courseNumber) const;
 };
